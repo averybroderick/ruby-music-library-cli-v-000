438b74eb76277bf235e75f7982706c7a0703b9a1
Avery Broderick <avery.c.broderick@gmail.com>
2018-01-21 14:43:40 +0000

    automatically backed up by learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -25,7 +25,7 @@ class MusicLibraryController
     counter = 1
     sorted_songs = Song.all.sort_by {|song| song.name}
     sorted_songs.each do |song|
-      puts "#{counter}. #{song.artist} - #{song.name} - #{song.genre}"
+      puts "#{counter}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
       counter += 1
     end
   end
