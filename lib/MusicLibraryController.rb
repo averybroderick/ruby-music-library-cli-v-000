9e46544c2f2844112335c4239ca0ab252ce9926d
Avery Broderick <avery.c.broderick@gmail.com>
2018-01-21 14:53:56 +0000

    automatically backed up by learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -50,7 +50,9 @@ class MusicLibraryController
 
   def list_songs_by_artist
     puts "Please enter the name of an artist:"
-    artist = gets.strip
+    artist_name = gets.strip
+    Artist.all.detect(|artist| artist.name == artist_name)
+
     sorted_songs = artist.songs.sort
     counter = 1
     sorted_songs.each do |song|
