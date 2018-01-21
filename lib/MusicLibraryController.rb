551ab4ea4473328f5bf2f0d278c16a818ac8fa33
Avery Broderick <avery.c.broderick@gmail.com>
2018-01-21 15:51:25 +0000

    automatically backed up by learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -20,7 +20,7 @@ class MusicLibraryController
     puts "What would you like to do?"
     answer = gets.strip until answer == 'exit'
     answer.split(" ").join("_")
-    self.instance_methods.each{|method| method.to_s == answer ? self.method : nil}
+    self.methods.each{|method| method.to_s == answer ? self.method : nil}
   end
 
   def list_songs
