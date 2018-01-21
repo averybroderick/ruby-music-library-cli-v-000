50b29d4853a736923e3d72b680a0dd09c8d467cd
Avery Broderick <avery.c.broderick@gmail.com>
2018-01-21 16:19:59 +0000

    automatically backed up by learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -18,7 +18,8 @@ class MusicLibraryController
     puts "To play a song, enter 'play song'."
     puts "To quit, type 'exit'."
     puts "What would you like to do?"
-    until gets.strip == 'exit'
+    answer = gets.strip
+    until answer == 'exit'
       answer = gets.strip
       case answer
       when "list songs"
