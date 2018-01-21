a6c87fca103ce887a96c2631ecd814de3f737e72
Avery Broderick <avery.c.broderick@gmail.com>
2018-01-21 14:49:37 +0000

    automatically backed up by learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -49,7 +49,7 @@ class MusicLibraryController
   end
 
   def list_songs_by_artist
-    puts "Please enter the name of an artist:"
+    "Please enter the name of an artist:"
     artist = gets.strip
   end
 
